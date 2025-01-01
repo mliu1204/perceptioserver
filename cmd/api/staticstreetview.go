ef286ff16cc7220aaca49a4e6cee9795a9e0d283
Michael Liu <michaelhongliu@gmail.com>
2024-12-31 18:14:05 -0700

    fixed naming issue (small change)

--- cmd/api/staticstreetview.go
+++ cmd/api/staticstreetview.go
@@ -12,7 +12,7 @@ func GetPictures(c *gin.Context) {
 	url := "https://maps.googleapis.com/maps/api/streetview?size=400x400&location=" + curLocation + "&fov=80&heading=70&pitch=0&key=AIzaSyCo4C8j7kGJNFnr4hjK3KrANonXc5Dq56c"
 	// fmt.Println(url)
 
-	signedURL, err := api.SignURL(url, os.Getenv("SECRET"))
+	signedURL, err := SignURL(url, os.Getenv("SECRET"))
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": "resource ID is required",
